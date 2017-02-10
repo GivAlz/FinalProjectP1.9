9feb9a259041b6ce9f8c958e198f21d2adc5a671
denocris <denobili16@gmail.com>
2017-02-10 16:54:55 +0100

    Updated python script

--- src/pyscript.py
+++ src/pyscript.py
@@ -1,7 +1,7 @@
 from ctypes import *
 import os, sys
 
-dso = CDLL('./ljmd_serial.so')
+dso = CDLL('../ljmd_serial.so')
 
 
 def read_file(inputfile):
@@ -12,12 +12,24 @@ def read_file(inputfile):
     restfile,trajfile,ergfile,line,nstep,dt = lines
 
     c_natoms = c_int(int(natoms))
+    c_nstep = c_int(int(float(nstep)))
+    c_dt = c_int(int(dt))
+    c_line = c_int(int(line))
+
+    c_mass = c_double(float(mass))
+    c_epsilon = c_double(float(epsilon))
     c_sigma = c_double(float(sigma))
+    c_rcut = c_double(float(rcut))
+    c_box = c_double(float(box))
+
     c_restfile = c_char_p(restfile)
+    c_trajfile = c_char_p(trajfile)
+    c_ergfile = c_char_p(ergfile)
 
-    dso.mklkop(c_)
 
-    return c_natoms, c_sigma, c_restfile.value
+    #dso.mklkop(c_)
+
+    return lines
 
 
 if __name__ == "__main__":
