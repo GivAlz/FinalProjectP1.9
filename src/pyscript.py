d48248f8124ef35824acef0f74599ed2760e1730
denocris <denobili16@gmail.com>
2017-02-10 16:33:07 +0100

    Starting python script for main

--- src/pyscript.py
+++ src/pyscript.py
@@ -1,6 +1,26 @@
-import ctypes
+from ctypes import *
+import os, sys
 
-# gcc -shared -Wl,-install_name,test1.so -o test1.so -fPIC test1.c
+dso = CDLL('./ljmd_serial.so')
 
-cprog = CDLL('./ljmd.so')
-cprog.test_few_atoms()
+
+def read_file(inputfile):
+    f = open(inputfile,"r")
+    lines = [line.rstrip('\n').split(" ")[0] for line in f]
+
+    natoms,mass,epsilon,sigma,rcut,box,\
+    restfile,trajfile,ergfile,line,nstep,dt = lines
+
+    c_natoms = c_int(int(natoms))
+    c_sigma = c_double(float(sigma))
+    c_restfile = c_char_p(restfile)
+
+    dso.mklkop(c_)
+
+    return c_natoms, c_sigma, c_restfile.value
+
+
+if __name__ == "__main__":
+
+    if(len(sys.argv)>1):
+        print read_file(sys.argv[-1])
